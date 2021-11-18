f7b6569fb48955fc0ad2d5e4c9ef55051768bf27
asathiya007 <akshay.sathiya@gmail.com>
2021-11-18 18:19:30 -0500

    wrote code to process data and extrapolate mean and variance information

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # sentiment-dataset-shift
 Course project for CS 8803 DMM (Data Management and Machine Learning) at Georgia Tech  (Fall 2021). Quantifies the dataset shift of sentiment classification datasets.
+
+# Getting Started 
+Create a Python virtual environment (with Python 3.8) and install the dependencies from the `./requirements.txt` file. 
+
+There are a set of default datasets in the `./datasets.zip` file. If using custom datasets, add them to the `./datasets` directory (produced by unzipping the `./datasets.zip` file) and specify custom data loading functions in the `./data.py` file. The `./data.py` file contains default data loading functions for the default datasets in the `./datasets` directory. 
+
+# Default Datasets
+The default datasets used in this paper are listed below. 
+
+1. IMDB
+
+Citation: Maas, A. L., Daly, R. E., Pham, P. T., Huang, D., Ng, A. Y., & Potts, C. (2011). Learning Word Vectors for Sentiment Analysis. Proceedings of the 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies, 142-150. http://www.aclweb.org/anthology/P11-1015. https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews  
+
+2. Movie Reviews 
+
+Citation: Pang, B., & Lee, L. (2004). A Sentimental Education: Sentiment Analysis Using Subjectivity Summarization Based on Minimum Cuts. Proceedings of the ACL. https://www.kaggle.com/nltkdata/movie-review?select=movie_review.csv  
+
+3. Emotions Dataset
+
+Citation: Saravia, E., Liu, H.T., Huang, Y., Wu, J., & Chen, Y. (2018). CARER: Contextualized Affect Representations for Emotion Recognition. Proceedings of the 2018 Conference on Empirical Methods in Natural Language Processing, 3687-3697. https://aclanthology.org/D18-1404. https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp?select=train.txt  
+
+4. Twitter 
+
+Citation: Merin, S. (2019). Twitter Emotion Analysis. Kaggle. Retrieved October 29, 2021, from https://www.kaggle.com/shainy/twitter-emotion-analysis/data  
+
+5. Financial News
+
+Citation: CrowdFlower. (2016, November 21). \textit{Sentiment Analysis in Text}. data.world. Retrieved November 2, 2021, from https://data.world/crowdflower/sentiment-analysis-in-text.
+
+6. Crowd Flower 
+
+Citation: Ayuya, C. (2020, November 30). \textit{Correcting Dataset Shift in Machine Learning}. Section. Retrieved November 2, 2021, from https://www.section.io/engineering-education/correcting-data-shift/. 
---
+++ data.py
@@ -0,0 +1,124 @@
+# import modules 
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import numpy as np
+import pandas as pd 
+from texthero import preprocessing, clean
+
+def default_posneg_loader(): 
+    # load IMDB data 
+    imdb_data = pd.read_csv('./datasets/IMDB/IMDBDataset.csv')
+    imdb_data['dataset'] = 'IMDB'
+    imdb_data['text'] = imdb_data['review']
+    imdb_data = imdb_data[['text', 'sentiment', 'dataset']]
+
+    # load movie reviews data 
+    movie_reviews_data = pd.read_csv('./datasets/MovieReviews/movie_review.csv')
+    movie_reviews_data['sentiment'] = movie_reviews_data['tag']
+    movie_reviews_data['dataset'] = 'Movie Reviews'
+    movie_reviews_data = movie_reviews_data[['text', 'sentiment', 'dataset']]
+
+    # load financial news data 
+    financial_news_data = pd.read_csv('./datasets/FinancialNews/all-data.csv', 
+        encoding="ISO-8859-1", header=None)
+    financial_news_data['text'] = financial_news_data[1]
+    financial_news_data['sentiment'] = financial_news_data[0]
+    financial_news_data['dataset'] = 'Financial News'
+    financial_news_data = financial_news_data[['text', 'sentiment', 'dataset']]
+
+    # concatenate datasets 
+    posneg_data = pd.concat([imdb_data, movie_reviews_data, financial_news_data], 
+        axis=0, ignore_index=True)
+
+    # return loaded datasets 
+    return posneg_data 
+    
+def default_emotions_loader(): 
+    # load emotions dataset 
+    emotions_data = []
+    file = open('./datasets/EmotionsDataset/train.txt')
+    line = file.readline()[:-1]
+    while line: 
+        chunks = line.split(';')
+        emotions_data.append(chunks)
+        line = file.readline()[:-1]
+    emotions_data = pd.DataFrame(emotions_data, columns=['text', 'sentiment'])
+    emotions_data['dataset'] = 'Emotions Dataset'
+
+    # load Twitter dataset 
+    twitter_data = pd.read_csv('./datasets/Twitter/data.csv')
+    twitter_data['text'] = twitter_data['Tweets']
+    twitter_data['sentiment'] = twitter_data['Feeling']
+    twitter_data['dataset'] = 'Twitter'
+    twitter_data = twitter_data[['text', 'sentiment', 'dataset']]
+
+    # load Crowd Flower dataset 
+    crowdflower_data = pd.read_csv('./datasets/CrowdFlower/text_emotion.csv')
+    crowdflower_data['text'] = crowdflower_data['content']
+    crowdflower_data['dataset'] = 'Crowd Flower'
+    crowdflower_data = crowdflower_data[['text', 'sentiment', 'dataset']]
+
+    emotions_data = pd.concat([emotions_data, twitter_data, crowdflower_data], 
+        axis=0, ignore_index=True)
+    
+    # return emotions dataset 
+    return emotions_data
+
+def vectorize_text(data, vector_size=100, epochs=20):
+    # clean text 
+    cleaning_pipeline = [
+        preprocessing.drop_no_content,
+        preprocessing.lowercase,
+        preprocessing.remove_whitespace,
+        preprocessing.remove_diacritics,
+        preprocessing.remove_brackets,
+        preprocessing.remove_stopwords, 
+        preprocessing.replace_punctuation
+    ]
+    data['vectorized_text'] = clean(data['text'], cleaning_pipeline)
+
+    # train Doc2Vec model
+    tagged_text = []
+    for i in range(len(data)):
+        tokens = data.loc[i, 'vectorized_text'].split(' ')
+        tagged_text.append(TaggedDocument(tokens, [i]))
+    model = Doc2Vec(vector_size=vector_size, min_count=1, epochs=epochs)
+    model.build_vocab(tagged_text)
+    model.train(tagged_text, total_examples=model.corpus_count, 
+        epochs=epochs)
+
+    # vectorize text 
+    vectorized_text = []
+    for i in range(len(data)):
+        tokens = data.loc[i, 'vectorized_text'].split(' ')
+        vectorized_text.append(list(model.infer_vector(tokens)))
+    data['vectorized_text'] = vectorized_text
+
+    # return data with vectorized text
+    return data 
+
+def extrapolate_distribution(data): 
+    # create multi-index 
+    data = data.set_index(['dataset', 'sentiment'])
+    data = data.sort_index()
+
+    # get multivariate mean and variance for each multi-index pair 
+    multi_indices = list(set(data.index))
+    vector_means = [] 
+    vector_variances = []
+    for dataset, sentiment in multi_indices: 
+        vectors = list(data.loc[(dataset, sentiment), 
+            'vectorized_text'])
+        vectors = np.array(vectors)
+        vector_means.append(list(np.mean(vectors, axis=0)))
+        vector_variances.append(list(np.var(vectors, axis=0)))
+
+    # consolidate information to mean table and variance table 
+    mean_info = pd.DataFrame(data=vector_means, 
+        index=pd.MultiIndex.from_tuples(multi_indices, 
+        names=['dataset', 'sentiment']))
+    variance_info = pd.DataFrame(data=vector_variances, 
+        index=pd.MultiIndex.from_tuples(multi_indices, 
+        names=['dataset', 'sentiment']))
+        
+    # return mean and variance information
+    return mean_info, variance_info
